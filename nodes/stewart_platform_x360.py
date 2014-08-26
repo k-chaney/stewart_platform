baac82152977a4e230a1af66dca1edc37b55ecdc
ken <ken@ken-Inspiron-7520.(none)>
2014-08-26 14:14:26 -0400

    added deadzone to x360 controller joy sticks to prevent unwanted drift and accidental motions

--- nodes/stewart_platform_x360.py
+++ nodes/stewart_platform_x360.py
@@ -16,6 +16,7 @@ class xbox_controller():
 		self.controllerMask = rospy.get_param('x360_dir_mask')
 		print self.controllerMapping
 		self.step_size = rospy.get_param('step_size')
+		self.deadZone = rospy.get_param('deadZone')
 		self.joy_data = None
 
 
@@ -45,29 +46,40 @@ class xbox_controller():
 			if self.joy_data:
 				if (self.getJoyData('axes','rt') < -0.9):
 					self.stewart_twist_pub.publish(self.Twist_In)
-				else:
-					self.stewart_pose.linear.x += self.getJoyData('axes','x') * self.step_size
-					self.stewart_pose.linear.y += self.getJoyData('axes','y') * self.step_size
-					self.stewart_pose.linear.z += self.getJoyData('axes','z') * self.step_size
-					self.stewart_pose.angular.z += self.getJoyData('axes','yaw') * self.step_size
-					self.stewart_twist_pub.publish(self.stewart_pose)
-				if (self.getJoyData('axes','lt') < -0.9):
+				if (self.getJoyData('button','b')==1): # deployed
 					self.stewart_pose.linear.x = 0.0
 					self.stewart_pose.linear.y = 0.0
 					self.stewart_pose.linear.z = 0.25
 					self.stewart_pose.angular.x = 0.0 # roll
 					self.stewart_pose.angular.y = 0.0 # pitch
 					self.stewart_pose.angular.z = 0.0 # yaw
+				if (self.getJoyData('button','a')==1): # stow
+					self.stewart_pose.linear.x = 0.0
+					self.stewart_pose.linear.y = 0.0
+					self.stewart_pose.linear.z = 0.05
+					self.stewart_pose.angular.x = 0.0 # roll
+					self.stewart_pose.angular.y = 0.0 # pitch
+					self.stewart_pose.angular.z = 0.0 # yaw
+				if (self.getJoyData('axes','rt')>-0.9):
+					self.stewart_pose.linear.x += self.getJoyData('axes','x') * self.step_size
+					self.stewart_pose.linear.y += self.getJoyData('axes','y') * self.step_size
+					self.stewart_pose.linear.z += self.getJoyData('axes','z') * self.step_size
+					self.stewart_pose.angular.z += self.getJoyData('axes','yaw') * self.step_size
 					self.stewart_twist_pub.publish(self.stewart_pose)
+
+
 
 
 			time.sleep(self.sleep_time)
 	def getJoyData(self,type,name):
 		if type=='axis' or type=='axes':
-			return self.joy_data.axes[self.controllerMapping[type][name]]*self.controllerMask[type][name]
+			if (abs(self.joy_data.axes[self.controllerMapping[type][name]])>self.deadZone):
+				return self.joy_data.axes[self.controllerMapping[type][name]]*self.controllerMask[type][name]
+			else:
+				return 0
 		elif type=='button':
 			return self.joy_data.button[self.controllerMapping[type][name]]*self.controllerMask[type][name]
-
+			
 if __name__ == '__main__':
 	try:
 		move_stewart = xbox_controller()
